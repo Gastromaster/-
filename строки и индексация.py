f63335c3d580a5e44352e512026efda495ef48bb
Gastromaster <165019499+Gastromaster@users.noreply.github.com>
2024-03-26 17:50:07 +0300

    Update строки и индексация.py

--- строки и индексация.py
+++ строки и индексация.py
@@ -2,7 +2,7 @@ name = "это моя строка"
 name2 = "это новая строка"
 print(name[0])
 print(name[-1])
-print(name[2:4])
+print(name[2:5])
 print(name[::-1])
 print(len(name))
 print(name2 + " " + name)
